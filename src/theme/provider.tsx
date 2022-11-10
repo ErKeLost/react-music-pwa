933a73400218b4d3f39267f111779861a402c6b3
ERKELSOT <1256029807@qq.com>
2022-11-10 17:49:42 +0800

    chore: 类型封装 redux

--- src/theme/provider.tsx
+++ src/theme/provider.tsx
@@ -1,19 +1,18 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 
 // import useTheme from '@/store/theme'
-import { shallowEqual } from 'react-redux'
-import { useMusicStoreSelector } from '~/store'
+import { useMusicSelector, shallowEqualMusic } from '~/store'
 import themes from './theme'
 import type { CustomThemeProviderProps } from './types'
 
 function CustomThemeProvider({ children }: CustomThemeProviderProps) {
   // const [theme] = useTheme()
-  const { mode } = useMusicStoreSelector(
+  const { mode } = useMusicSelector(
     (state) => ({
       mode: state.theme.mode
     }),
     // 数据没有发生变化 就不会自动刷新了
-    shallowEqual
+    shallowEqualMusic
   )
   return (
     // @ts-ignore
